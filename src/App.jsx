2fbea036c43229cb2dfe191d31f44dcd0afa1899
Priyanshu Pandey <priyanshun14314@gmail.com>
2025-05-25 16:28:09 +0530

    Added css, props and usestate

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Square from "./Square";
 export default function Board() {
   return <>
     <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={1}/>
+      <Square value={2}/>
+      <Square value={3}/>
     </div>
     <div className="board-row">
       <Square/>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 const Square = () => {
+  const [value, setValue] = useState(null);
+  function handleClick(){
+    setValue('X');
+  }
   return (
-     <button className="square">1</button>
+     <button className="square" onClick={handleClick}>{value}</button>
   )
 }
 export default Square
